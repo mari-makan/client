18d1d7d35648339034286af197d3854dad1ad600
tamarazu <tamarazulaika.u@gmail.com>
2020-02-07 09:16:02 +0700

    fix main.js

--- main.js
+++ main.js
@@ -1,77 +1,31 @@
-localStorage.setItem('token', '123456')
+// localStorage.setItem('token', '123456')
 // localStorage.clear()
 
 function hideAll(){
-    $("#landing-page").hide()
+    $("#home").hide()
     $("#formSignIn").hide()
-<<<<<<< HEAD
-    $("#menu").hide()
-=======
     $("#formSignUp").hide()
-<<<<<<< HEAD
-    $("#carouselExampleCaptions").hide()
-=======
->>>>>>> origin
->>>>>>> development
 }
-$(document).ready(() => {
-<<<<<<< HEAD
-    hideAll()
-    $("#formSignIn").show()
-    $("#formSignUp").hide()
-    // if(localStorage.token !== undefined){
-    //     // console.log(`masuk`)
-    //     hideAll()
-    //     $("#check").show()
-=======
-<<<<<<< HEAD
-    // hideAll()
-    // hideAll()
-    // // $("#menu").hide()
-    // if(localStorage.token){
-    //     hideAll()
-    //     $("#landing-page").show()
->>>>>>> development
-    // } else {
-    //     hideAll()
-    //     $("#formSignIn").show()
-    // }
-<<<<<<< HEAD
-=======
 
-    // LOGIN
-    // $("#signIn").on("submit", () => {
-    //     hideAll()
-    //     // DUMMY TOKEN  ================================
-    //     localStorage.setItem('token', '123456')
-    //     $("#landing-page").show()
-    // })
-=======
+$(document).ready(() => {
 
     if(localStorage.token !== undefined){
-        console.log(`masuk`)
+        // console.log(`masuk`)
         hideAll()
         $("#check").show()
     } else {
         hideAll()
         $("#formSignIn").show()
     }
->>>>>>> development
 
     // LOGIN
-
-    $('#showForm').on("click", (event) => {
-
-    })
-
     $("#signIn").on("submit", (e) => {
         // e.preventDefault()
         hideAll()
         console.log(`masuk`)
         // DUMMY TOKEN  ================================
         localStorage.setItem('token', '123456')
-        $("#landing-page").show()
+        $("#home").show()
     })
 
->>>>>>> origin
 })
