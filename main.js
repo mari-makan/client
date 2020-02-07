edba7c498a9e9b760acd01eec986adb44efec532
adamjay041 <adamjay041@gmail.com>
2020-02-07 08:59:30 +0700

    navbar

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-// localStorage.setItem('token', '123456')
+localStorage.setItem('token', '123456')
 // localStorage.clear()
 
 function hideAll(){
-    $("#home").hide()
+    $("#landing-page").hide()
     $("#formSignIn").hide()
+    $("#menu").hide()
 }
-
 $(document).ready(() => {
-
-    if(localStorage.token){
-        hideAll()
-        $("#home").show()
-    } else {
-        hideAll()
-        $("#formSignIn").show()
-    }
+    // hideAll()
+    // hideAll()
+    // // $("#menu").hide()
+    // if(localStorage.token){
+    //     hideAll()
+    //     $("#landing-page").show()
+    // } else {
+    //     hideAll()
+    //     $("#formSignIn").show()
+    // }
 
     // LOGIN
-    $("#signIn").on("submit", () => {
-        hideAll()
-        // DUMMY TOKEN  ================================
-        localStorage.setItem('token', '123456')
-        $("#home").show()
-    })
+    // $("#signIn").on("submit", () => {
+    //     hideAll()
+    //     // DUMMY TOKEN  ================================
+    //     localStorage.setItem('token', '123456')
+    //     $("#landing-page").show()
+    // })
 })
