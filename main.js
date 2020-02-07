2d7af98c06ebd3017b89ddd4f91c493a5c7729c5
tamarazu <tamarazulaika.u@gmail.com>
2020-02-07 09:07:32 +0700

    changes at main.js

--- main.js
+++ main.js
@@ -2,30 +2,38 @@
 // localStorage.clear()
 
 function hideAll(){
-    $("#home").hide()
+    $("#landing-page").hide()
     $("#formSignIn").hide()
     $("#formSignUp").hide()
+    $("#carouselExampleCaptions").hide()
 }
 
 $(document).ready(() => {
-
-    if(localStorage.token !== undefined){
-        console.log(`masuk`)
-        hideAll()
-        $("#check").show()
-    } else {
-        hideAll()
-        $("#formSignIn").show()
-    }
+    hideAll()
+    $("#formSignIn").show()
+    $("#formSignUp").hide()
+    // if(localStorage.token !== undefined){
+    //     // console.log(`masuk`)
+    //     hideAll()
+    //     $("#check").show()
+    // } else {
+    //     hideAll()
+    //     $("#formSignIn").show()
+    // }
 
     // LOGIN
+
+    $('#showForm').on("click", (event) => {
+
+    })
+
     $("#signIn").on("submit", (e) => {
         // e.preventDefault()
         hideAll()
         console.log(`masuk`)
         // DUMMY TOKEN  ================================
         localStorage.setItem('token', '123456')
-        $("#home").show()
+        $("#landing-page").show()
     })
 
 })
